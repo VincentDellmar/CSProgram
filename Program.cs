7c98482b6cd27a16ee21892c6e7c2989929b345c
Vincent Dellmar <vwdel826@learner.dvusd.org>
2022-08-10 12:36:04 -0700

    Yes
    Yes

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? NEW_STRING = Console.ReadLine();
+        if (NEW_STRING == "YES")
+        {
+            Console.WriteLine("Wow very epic answer you made!!!"); 
+        } else
+        {
+             Console.WriteLine(":("); 
+        }
+    }
+}
+
+
